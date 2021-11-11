46e46c7898848ea2a9b3817176c8101cef277138
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-11-11 19:32:28 +0300

    Stage 2 homework 17

---
+++ src/main/kotlin/stage2/hw17/Main.kt
@@ -0,0 +1,9 @@
+package stage2.hw17
+
+fun subtractTwoNumbers(a: Long, b: Long) = println(a - b)
+
+fun sumTwoNumbers(a: Long, b: Long) = println(a + b)
+
+fun divideTwoNumbers(a: Long, b: Long) = println(if (b == 0L) "Division by 0!" else a / b)
+
+fun multiplyTwoNumbers(a: Long, b: Long) = println(a * b)
