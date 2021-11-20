36228c6a61fb88fd62c9b4e55baba50dffab44eb
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-11-21 00:06:44 +0300

    Stage 2 homework 31 fix NPE

--- src/main/kotlin/stage2/hw31/Main.kt
+++ src/main/kotlin/stage2/hw31/Main.kt
@@ -2,12 +2,17 @@ package stage2.hw31
 
 import java.io.File
 
+/**
+ * Find empty subdirs under top-level dirs in basedir
+ */
 fun main() {
     val basedir = File("basedir")
 
     for (dir in basedir.listFiles()) {
         for (subdir in dir.listFiles()) {
-            if (subdir.list().isEmpty()) println(subdir.name)
+            subdir.list()?.let {
+                if (it.isEmpty()) println(subdir.name)
+            }
         }
     }
 }
