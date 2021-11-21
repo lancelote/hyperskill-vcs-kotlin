6447758d595ac05a75b7ff99a41e860fdff9d593
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-11-21 14:39:33 +0300

    Template for "config" and "add" functions

--- src/main/kotlin/stage2/project/Main.kt
+++ src/main/kotlin/stage2/project/Main.kt
@@ -1,6 +1,6 @@
 package stage2.project
 
-val COMMANDS = mapOf(
+val COMMANDS_HELP = mapOf(
     "config" to "Get and set a username.",
     "add" to "Add a file to the index.",
     "log" to "Show commit logs.",
@@ -11,14 +11,20 @@ val COMMANDS = mapOf(
 fun printHelp() {
     println("These are SVCS commands:")
 
-    for ((k, v) in COMMANDS) {
-        println("${k.padEnd(10)} $v")
+    for ((commandName, helpLine) in COMMANDS_HELP) {
+        println("${commandName.padEnd(10)} $helpLine")
     }
 }
+
+fun config() {}
+
+fun add() {}
 
 fun main(args: Array<String>) {
     when (val command = args.firstOrNull()) {
         "--help" -> printHelp()
-        else -> println(COMMANDS[command] ?: "'$command' is not a SVCS command.")
+        "config" -> config()
+        "add" -> add()
+        else -> println("'$command' is not a SVCS command.")
     }
 }
