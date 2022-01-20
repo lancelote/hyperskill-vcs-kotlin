e970bf23012cf89684fbc6864fa5c81042ad2a7d
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-01-20 21:36:52 +0300

    Stage 3 homework 3

---
+++ src/main/kotlin/stage3/hw3/Main.kt
@@ -0,0 +1,10 @@
+package stage3.hw3
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val num1 = scanner.nextLong()
+    val num2 = scanner.nextLong()
+    println(num1 + num2)
+}
