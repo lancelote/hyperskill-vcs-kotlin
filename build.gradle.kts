28734e2f7a05c46c9aa759450bed11073f169d88
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-07-03 23:53:37 +0200

    Bump JUnit version to 5.10.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
 }
 
 tasks.test {
